569101dbb99ce96406cb03eb5be03407f657f3ff
Jaspaul Bola <jaspaul.b@gmail.com>
2014-08-06 10:50:30 -0400

    Basic implementation of Login for LDAP strategy

--- src/SoapBox/AuthorizeLdap/LdapStrategy.php
+++ src/SoapBox/AuthorizeLdap/LdapStrategy.php
@@ -1,11 +1,29 @@
 <?php namespace SoapBox\AuthorizeLdap;
 
+use SoapBox\Authorize\User;
 use SoapBox\Authorize\Strategyies\SingleSignOnStrategy;
 
-class LdapStrategy extends SingleSignOnStrategy {
+class LdapStrategy extends Strategy {
+
+	const QUERY_STRING = '(&(objectClass=%s)(%s=%s))';
 
 	private $connection;
 	private $application = [];
+
+	/**
+	 * Returns the default if the value is not set.
+	 *
+	 * @param $value mixed The value you wish to validate.
+	 * @param $default mixed The value you wish to get if value is not set
+	 *
+	 * @return mixed
+	 */
+	private function getValueOrDefault($value, $default) {
+		if (isset($value)) {
+			return $value;
+		}
+		return $default;
+	}
 
 	public function __construct($settings = array()) {
 		if (!isset($settings['connection']['url']) ||
@@ -47,4 +65,59 @@ class LdapStrategy extends SingleSignOnStrategy {
 		ldap_set_option($this->connection, LDAP_OPT_REFERRALS, 0);
 	}
 
+	public function login($parameters = array()) {
+		if (!isset($parameters['username']) ||
+			!isset($parameters['password']) ||
+			!isset($parameters['fields']) ||
+			!is_array($parameters['fields'])) {
+			throw new \Exception('Username and password are required to login.');
+		}
+
+		$query = sprintf(
+			QUERY_STRING,
+			'user',
+			$this->application['searchName'],
+			$username
+		);
+
+		$status = @ldap_search(
+			$this->connection,
+			$this->application['searchBase'],
+			$query,
+			array_values($parameters['fields'])
+		);
+
+		if ($status === false) {
+			throw new \Exception('LDAP search failed');
+		}
+
+		$result = ldap_get_entries($this->connection, $status);
+
+		if ($result === false) {
+			throw new \Exception('Could not retrieve results from LDAP server');
+		}
+
+		$user = new User;
+
+		if ((int) @$result['count'] !== 1) {
+			$result = $result[0];
+			$user->id = $this->getValueOrDefault($result['samaccountname'][0], '');
+			$user->displayName = $this->getValueOrDefault($result['dn'], '');
+			$user->username = $this->getValueOrDefault($result['username'][0], null);
+			$user->email = $this->getValueOrDefault($result['mail'][0], null);
+			$user->accessToken = 'token';
+			$user->firstname = $this->getValueOrDefault($result['givenname'][0], '');
+			$user->lastname = $this->getValueOrDefault($result['sn'][0], '');
+
+			if($user->email == null) {
+				$user->email = $this->getValueOrDefault($result['userprincipalname'][0], null);
+			}
+		}
+
+		if ($user->displayName === '') {
+			throw new \Exception('Something went wrong');
+		}
+
+		return $user;
+	}
 }
